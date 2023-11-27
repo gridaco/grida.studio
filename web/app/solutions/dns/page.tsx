0cc8153617c88974152f332b3bd6bf436fd60b93
Universe <universe@grida.co>
2023-11-27 14:50:27 +0900

    init solutions

---
+++ web/app/solutions/dns/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function DnsSolutionPage() {
+  return <main>
+    <h1>DNS Solution</h1>
+    <Link href="/https://payments.grida.co/b/14k8wN5X67Z61I4eVb">Full DNS Management with $125/Querter</Link>
+  </main>
+}
