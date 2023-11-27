85dc0d21b0530889b969a808b238aa6c9e7ce3d9
Universe <universe@grida.co>
2023-11-27 14:54:21 +0900

    style

--- web/app/solutions/dns/page.tsx
+++ web/app/solutions/dns/page.tsx
@@ -5,9 +5,20 @@ export default function DnsSolutionPage() {
     <header className="mt-40">
       <h1 className="text-4xl font-bold">Grida Studios DNS Solutions</h1>
     </header>
-    <p>
-      Grida Studios provides DNS solutions for your domain. We can provide you with a DNS server for your domain, or you can use our DNS server.
-    </p>
-    <Link href="/https://payments.grida.co/b/14k8wN5X67Z61I4eVb">Full DNS Management with $125/Querter</Link>
+    <div className="mt-10">
+      <p className="py-4">
+        Grida Studios provides DNS solutions for your domain. We can provide you with a DNS server for your domain, or you can use our DNS server.
+        <ul>
+          <li>— Gmail Setup</li>
+          <li>— DNS Record Management</li>
+          <li>— Website Hosting</li>
+        </ul>
+      </p>
+      <Link href="/https://payments.grida.co/b/14k8wN5X67Z61I4eVb">
+        <button className="bg-black text-white px-4 py-2 rounded shadow-sm">
+          Full DNS Management with $125/Querter
+        </button>
+      </Link>
+    </div>
   </main>
 }
