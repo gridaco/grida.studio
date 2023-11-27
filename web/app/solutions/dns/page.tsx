8b97381feadded149b72c6c6f6145ddbe62c3a0f
Universe <universe@grida.co>
2023-11-27 14:51:55 +0900

    .

--- web/app/solutions/dns/page.tsx
+++ web/app/solutions/dns/page.tsx
@@ -2,7 +2,12 @@ import Link from "next/link";
 
 export default function DnsSolutionPage() {
   return <main>
-    <h1>DNS Solution</h1>
+    <header className="mt-40">
+      <h1 className="text-4xl font-bold">Grida Studios DNS Solutions</h1>
+    </header>
+    <p>
+      Grida Studios provides DNS solutions for your domain. We can provide you with a DNS server for your domain, or you can use our DNS server.
+    </p>
     <Link href="/https://payments.grida.co/b/14k8wN5X67Z61I4eVb">Full DNS Management with $125/Querter</Link>
   </main>
 }
